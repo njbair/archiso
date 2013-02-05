a874dca5286eeab5981cc877b679206da94fc442
Nick Bair <njbair@gmail.com>
2013-02-04 19:56:57 -0500

    Customized for personal use

--- configs/releng/root-image/root/customize_root_image.sh
+++ configs/releng/root-image/root/customize_root_image.sh
@@ -11,13 +11,14 @@ usermod -s /bin/zsh root
 cp -aT /etc/skel/ /root/
 
 useradd -m -p "" -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -s /bin/zsh arch
+useradd -m -g users -G "adm,audio,floppy,log,network,rfkill,scanner,storage,optical,power,wheel" -d /home/njbair -s /bin/zsh njbair
 
 chmod 750 /etc/sudoers.d
-chmod 440 /etc/sudoers.d/g_wheel
+chmod 440 /etc/sudoers.d/*
 
 sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
 
-sed 's#\(^ExecStart=-/sbin/agetty\)#\1 --autologin root#;
+sed 's#\(^ExecStart=-/sbin/agetty\)#\1 --autologin njbair#;
      s#\(^Alias=getty.target.wants/\).\+#\1autologin@tty1.service#' \
      /usr/lib/systemd/system/getty@.service > /etc/systemd/system/autologin@.service
 
